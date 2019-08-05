d9713f1a3c273afeefabd5fe94179ed5ec5ee870
arturoQC <arturoquinterocortes@gmail.com>
2019-08-04 21:49:13 -0400

    last commit

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module IntroduccionRorJuanYArturo
   class Application < Rails::Application
+     # config/application.rb config.assets.initialize_on_precompile = false 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
